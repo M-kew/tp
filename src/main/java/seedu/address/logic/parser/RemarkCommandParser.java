606509c4c2a7814c873caddae5086a7999ae1f15
M-kew <e0958776@u.nus.edu>
2024-10-04 14:31:14 +0800

    Add remark command to the addressbook

--- src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -30,4 +30,5 @@ public class RemarkCommandParser implements Parser<RemarkCommand> {
         String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
         return new RemarkCommand(index, new Remark(remark));
     }
+
 }
